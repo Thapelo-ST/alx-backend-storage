0c0926347390fd6d2def1a91e2fe5d090563f817
Thapelo-ST <tsamkelo96@gmal.com>
2024-01-23 17:55:05 -0800

    check

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' lists all documents in collection '''
+
+
+def list_all(mongo_collection):
+	''' listing all the documents in a MongoDB collection'''
+	finder = mongo_collection.find({})
+
+	documents = list(finder)
+
+	return documents
