fbe1a3742cae2282aa1e4822e0bb3114e3081a9d
Thapelo-ST <tsamkelo96@gmail.com>
2024-01-25 00:18:59 +0000

    help

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,29 @@
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
+
+def count_calls(method: Callable = None) -> Callable:
+    ''' count calls of the cache '''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+def call_history(method: Callable = None) -> Callable:
+    ''' count calls of the cache '''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+	inputs_key = "{}:inputs".format(key)
+	outputs_key = "{}:outputs".format(key)
+	self._redis.rpush(inputs_key, str(args))
+	output = method(self, *args, **kwargs)
+	self._redis.rpush(outputs_key, str(output))
+        return output
+    return wrapper
 
 
 class Cache:
@@ -15,7 +38,9 @@ class Cache:
         ''' inititialising the class '''
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         ''' takes data and returns a string key'''
         key = str(uuid.uuid4())
@@ -33,4 +58,4 @@ class Cache:
     
     def get_int(self, key: str) -> int:
         ''' parameetarise cache.get and converts it into an int '''
-        return self.get(key, fn=lambda x: int(x) if x else None 
+        return self.get(key, fn=lambda x: int(x) if x else None) 
